07592745949ac8c3216c20ef51d8f5941ed14940
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-10 11:21:07 +0600

    navbar fixed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-"use strict";
-console.log("Hello Noman");
+var barIcon = document.querySelector(".bar");
+var headerButtons = document.querySelector(".HeaderFormbl .HeaderButtons");
+// Add a click event listener to the bar icon
+barIcon === null || barIcon === void 0 ? void 0 : barIcon.addEventListener("click", function () {
+    // Toggle the classes of the bar icon
+    barIcon.classList.toggle("fa-bars");
+    barIcon.classList.toggle("fa-xmark");
+    // Toggle the 'active' class for HeaderButtons container
+    headerButtons.classList.toggle("active");
+    console.log(headerButtons);
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello Noman");
+const barIcon = document.querySelector(".bar");
+const headerButtons = document.querySelector(".HeaderFormbl .HeaderButtons")!;
+
+// Add a click event listener to the bar icon
+barIcon?.addEventListener("click", function () {
+  // Toggle the classes of the bar icon
+  barIcon.classList.toggle("fa-bars");
+  barIcon.classList.toggle("fa-xmark");
+  // Toggle the 'active' class for HeaderButtons container
+  headerButtons.classList.toggle("active");
+  console.log(headerButtons);
+});
